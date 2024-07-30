e2ea4dcb6c7ce3013f6be59c7eac10cba0285abe
UzielCarranza <uzielcarranzac97@gmail.com>
2024-07-29 20:34:17 -0400

    default configuraiton

---
+++ src/main/java/com/app/amigojobs/AmigoJobsApplication.java
@@ -0,0 +1,13 @@
+package com.app.amigojobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoJobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/amigojobs/AmigoJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.amigojobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
